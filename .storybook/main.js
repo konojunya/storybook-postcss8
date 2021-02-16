bdbe56019b3580b1c72cf2133ef6e74444e2a8fe
konojunya <junya03dance@gmail.com>
2021-02-16 13:10:22 +0900

    できた

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,14 +4,23 @@ const APP_ROOT = path.resolve(__dirname, '../');
 
 module.exports = {
   stories: [path.join(APP_ROOT, '**/*.stories.tsx')],
-  addons: [
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-  ],
+  addons: [],
+  webpackFinal: async (config) => {
+    config.module.rules.forEach(rule => {
+      if (rule.test.toString() === /\.css$/.toString()) {
+        rule.use.forEach(using => {
+          if(using.loader && using.loader.match(/\/css-loader/) != null) {
+            using.options = {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+              }
+            }
+          }
+        })
+      }
+    })
+
+    return config;
+  }
 };
